2b353d35c2b52d2bc0fe2da9af7b5e10c6cbd671
nontallas <n.o.ntallas@gmail.com>
2020-05-14 18:20:04 +0300

    Minor style changes in gui/app

--- src/gui/app.java
+++ src/gui/app.java
@@ -13,8 +13,8 @@ import org.eclipse.swt.widgets.Button;
 class app {
 
 	protected Shell shlWikipediaSearchEngine;
-	private Text txtPleaseInsertYour;
-	private Text text;
+	private Text txtQuery;
+	private Text txtResults;
 	private Text txtHistory;
 
 	/**
@@ -55,44 +55,43 @@ class app {
 		shlWikipediaSearchEngine.setText("Wikipedia Search Engine");
 		shlWikipediaSearchEngine.setLayout(null);
 		
-		txtPleaseInsertYour = new Text(shlWikipediaSearchEngine, SWT.BORDER);
-		txtPleaseInsertYour.setText("Please insert your query");
-		txtPleaseInsertYour.setBounds(100, 62, 881, 29);
+		txtQuery = new Text(shlWikipediaSearchEngine, SWT.NONE);
+		txtQuery.setBounds(100, 62, 881, 29);
 		
-		Button btnNewButton = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton.setFont(SWTResourceManager.getFont("Ubuntu", 11, SWT.NORMAL));
-		btnNewButton.setBounds(100, 105, 110, 29);
-		btnNewButton.setText("Default Search");
+		Button btnDefaultSearch = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnDefaultSearch.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
+		btnDefaultSearch.setBounds(100, 105, 110, 29);
+		btnDefaultSearch.setText("Default Search");
 		
-		Button btnNewButton_1 = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton_1.setBounds(248, 105, 110, 29);
-		btnNewButton_1.setText("Title Search");
+		Button btnTitleSearch = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnTitleSearch.setBounds(248, 105, 110, 29);
+		btnTitleSearch.setText("Title Search");
 		
-		Button btnNewButton_2 = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton_2.setBounds(389, 105, 110, 29);
-		btnNewButton_2.setText("Enable History");
+		Button btnHistory = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnHistory.setBounds(389, 105, 110, 29);
+		btnHistory.setText("Enable History");
 		
-		Button btnNewButton_3 = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton_3.setFont(SWTResourceManager.getFont("Ubuntu", 9, SWT.NORMAL));
-		btnNewButton_3.setBounds(871, 105, 110, 29);
-		btnNewButton_3.setText("Alphabetical Sort");
+		Button btnABSort = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnABSort.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
+		btnABSort.setBounds(871, 105, 110, 29);
+		btnABSort.setText("Alphabetical Sort");
 		
-		Button btnNewButton_4 = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton_4.setBounds(728, 105, 110, 29);
-		btnNewButton_4.setText("Sort by Size");
+		Button btnSizeSort = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnSizeSort.setBounds(728, 105, 110, 29);
+		btnSizeSort.setText("Sort by Size");
 		
-		Button btnNewButton_5 = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton_5.setBounds(576, 105, 110, 29);
-		btnNewButton_5.setText("Sort by Score");
+		Button btnScoreSort = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnScoreSort.setBounds(576, 105, 110, 29);
+		btnScoreSort.setText("Sort by Score");
 		
-		Button btnNewButton_6 = new Button(shlWikipediaSearchEngine, SWT.NONE);
-		btnNewButton_6.setBounds(492, 158, 93, 29);
-		btnNewButton_6.setText("Search");
+		Button btnSearch = new Button(shlWikipediaSearchEngine, SWT.NONE);
+		btnSearch.setBounds(492, 158, 93, 29);
+		btnSearch.setText("Search");
 		
-		text = new Text(shlWikipediaSearchEngine, SWT.BORDER);
-		text.setBounds(100, 206, 675, 374);
+		txtResults = new Text(shlWikipediaSearchEngine, SWT.NONE);
+		txtResults.setBounds(100, 206, 675, 374);
 		
-		txtHistory = new Text(shlWikipediaSearchEngine, SWT.BORDER);
+		txtHistory = new Text(shlWikipediaSearchEngine, SWT.NONE);
 		txtHistory.setFont(SWTResourceManager.getFont("Ubuntu", 11, SWT.NORMAL));
 		txtHistory.setBounds(781, 206, 200, 374);
 		
@@ -101,17 +100,25 @@ class app {
 		btnNewButton_7.setText("Help");
 		
 		Label lblNewLabel = new Label(shlWikipediaSearchEngine, SWT.NONE);
+		lblNewLabel.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 		lblNewLabel.setAlignment(SWT.RIGHT);
-		lblNewLabel.setBounds(0, 674, 1074, 17);
+		lblNewLabel.setBounds(-22, 653, 1074, 20);
 		lblNewLabel.setText("Created as a course project for the Information Retrieval course in UoI");
 		
 		Label lblNewLabel_1 = new Label(shlWikipediaSearchEngine, SWT.NONE);
+		lblNewLabel_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 		lblNewLabel_1.setBounds(100, 183, 200, 17);
 		lblNewLabel_1.setText("Results");
 		
 		Label lblNewLabel_2 = new Label(shlWikipediaSearchEngine, SWT.NONE);
+		lblNewLabel_2.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 		lblNewLabel_2.setBounds(781, 183, 200, 17);
 		lblNewLabel_2.setText("History");
+		
+		Label lblPleaseInsertYour = new Label(shlWikipediaSearchEngine, SWT.NONE);
+		lblPleaseInsertYour.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		lblPleaseInsertYour.setBounds(100, 36, 172, 20);
+		lblPleaseInsertYour.setText("Please insert your query");
 
 	}
 }
